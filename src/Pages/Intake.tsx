dac3c5bee648692e46eb708502dfb1c375fcc878
marquisejaybyrd <marquise.byrd@bison.howard.edu>
2023-02-04 20:24:10 -0500

    added this page for debugging

---
+++ src/Pages/Intake.tsx
@@ -0,0 +1,30 @@
+import './Intake.css';
+import React from 'react';
+const Intake = () => {
+        return (
+            <div>
+                <div className="MainPage">
+                    <div className='WelcomeText'>
+                        Welcome to The Hospital Triage!
+                    </div>
+                    <div className='blackButtonContainer'>
+                        <button className='BlackButton'>Personal Information</button>
+                        <button className='BlackButton'>Nearby Hospitals</button>
+                    </div>
+                    
+                    <div>Forms</div>
+                    <div>
+                        <button className='GreyButton'>NEW INTAKE FORM</button>
+                        <button className='GreyButton'>UPDATE MEDICAL HISTORY</button>
+                        <button className='GreyButton'>VIEW PREVIOUS INTAKE FORMS</button>
+                        <button className='GreyButton'>VIEW MEDICAL HISTORY</button>
+                        <button className='GreyButton'>CLICK HERE TO UPLOAD FORMS</button>
+                    </div>
+                    
+                </div>
+            </div>
+        )
+}
+
+
+export default Intake;
